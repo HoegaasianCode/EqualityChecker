66b98003e592e76ee6494ef96a8a4f4381f633a0
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 16:40:44 +0100

    Initial commit

---
+++ EqualityCheck1/EqualityChecker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EqualityCheck1
+{
+    public class EqualityChecker
+    {
+        private readonly object _x;
+        private readonly object _y;
+        public bool CheckTypeAndValue() => _x.Equals(_y);
+        public EqualityChecker(object x, object y)
+        {
+            _x = x;
+            _y = y;
+        }
+    }
+}
---
+++ EqualityCheck1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EqualityCheck1
+{
+    class Program
+    {
+        // https://edabit.com/challenge/Q7g8sfg7DJq6CyMMu
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            var types = new EqualityChecker("0", 0);
+            Console.WriteLine(types.CheckTypeAndValue());
+        }
+    }
+}
